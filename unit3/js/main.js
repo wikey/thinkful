4d5a31e65237790d70675d593c5fa6ea0f308432
Ian Sullivan <ian@churchkey.org>
2015-07-07 19:12:09 -0400

    Working: adding value of input field into properly formatted css emelent for the todo list. The item added previously was 'undefined' because I had the variables defined further down in the document than I was calling them

--- unit3/js/main.js
+++ unit3/js/main.js
@@ -1,17 +1,19 @@
 $(function(){
     $("#newTodo").on("keydown",function(){
 	if (event.which == 13){
+	    var preTodo = '<div class="itembox"><div class="itemleft">'
+	    var textTodo = $("#newTodo").val();
+	    /*var textTodo = document.getElementById('newTodo').value;*/
+	    var postTodo = '</div><div class="itemright"></div></div>'
+	    var valueTodo = preTodo + textTodo + postTodo
+
 	    /*	    console.log("enter pressed");*/
 	    /*	    $(".todos").append(valueTodo);*/ /*The CSS works for this part but it cannot read the value of the input. All new todos are set as "undefined".*/
 	    $(".todos").append(valueTodo);	    
+	    $("#newTodo").find('input:text').val('');
 	}
 	
     }
 		   );
 })
 
-var preTodo = "<div class=\"itembox\"><div class=\"itemleft\">"
-var textTodo = $('#newTodo').val();
-/*var textTodo = document.getElementById('newTodo').value;*/
-var postTodo = "</div><div class=\"itemright\"></div></div>"
-var valueTodo = preTodo + textTodo + postTodo
