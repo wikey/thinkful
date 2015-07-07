233c214b12f6b91b9df1ae22fd7a3cdccf02356a
Ian Sullivan <ian@churchkey.org>
2015-07-07 19:13:49 -0400

    Works: clearing the value of the input field upon adding it as a todo

--- unit3/js/main.js
+++ unit3/js/main.js
@@ -6,11 +6,9 @@ $(function(){
 	    /*var textTodo = document.getElementById('newTodo').value;*/
 	    var postTodo = '</div><div class="itemright"></div></div>'
 	    var valueTodo = preTodo + textTodo + postTodo
-
-	    /*	    console.log("enter pressed");*/
-	    /*	    $(".todos").append(valueTodo);*/ /*The CSS works for this part but it cannot read the value of the input. All new todos are set as "undefined".*/
 	    $(".todos").append(valueTodo);	    
-	    $("#newTodo").find('input:text').val('');
+	    /*	    $("#newTodo").find('input:text').val('');*/
+	    $("#newTodo").val('');
 	}
 	
     }
