8195f97c49d5ceba67176d80837ebcfbf9db2fa7
Ian Sullivan <ian@churchkey.org>
2015-07-09 12:37:32 -0400

    Working: fizzBuzz function. Broken: triggering the fizzBuzz function based on .button click and appending the output to the .runme div

--- unit4/js/main.js
+++ unit4/js/main.js
@@ -1,13 +1,31 @@
 $(function(){
-    $(".botton").on("click",function(){
+    $(".button").on("click",function(){
+	var the100 = fizzBuzz(1,100)
 	$(".runme").append(the100)
     });
 
 
 })
 
-var the100 = fizzBuzz(1,100)
-
-function fizzBuzz(start,end){
-    
+function fizzBuzz(){
+    var fbvalue = [];
+    var fbreturn = "";
+    for(var i = 1; i <= 100; i++){
+	if(i%3 == 0 && i%5 == 0){
+	    fbvalue.push("fizbuzz")
+	}
+	else if(i%3 == 0){
+	    fbvalue.push("fizz")
+	}
+	else if(i%5 == 0){
+	    fbvalue.push("buzz")
+	}
+	else{fbvalue.push(i)}
+    }
+    fbreturn = fbvalue.join(", ");
+    console.log(fbreturn);
 }
+
+var the100 = fizzBuzz(1,100);
+
+
