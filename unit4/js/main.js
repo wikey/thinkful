6b40e043e6edd5277563e63edcb2fc072ce4cefa
Ian Sullivan <ian@churchkey.org>
2015-07-13 18:15:07 -0400

    Challenge completed. Have to keep the order of calls straight. Earlier failures were caused by including my custom js file before the jquery <script> tag, thus causing my event listener to be undefined

--- unit4/js/main.js
+++ unit4/js/main.js
@@ -1,15 +1,12 @@
 $(function(){
     $(".button").on("click",function(){
-	var the100 = fizzBuzz(1,100)
-	$(".runme").append(the100)
-    });
-
-
+	/*using .empty() here to prevent flooding the box on subsequent clicks*/
+	$(".runme").empty().append("<p>",fizzBuzz(1,100),"</p>");
+	});
 })
 
 function fizzBuzz(){
     var fbvalue = [];
-    var fbreturn = "";
     for(var i = 1; i <= 100; i++){
 	if(i%3 == 0 && i%5 == 0){
 	    fbvalue.push("fizbuzz")
@@ -22,10 +19,5 @@ function fizzBuzz(){
 	}
 	else{fbvalue.push(i)}
     }
-    fbreturn = fbvalue.join(", ");
-    console.log(fbreturn);
+    return(fbvalue.join(", "));
 }
-
-var the100 = fizzBuzz(1,100);
-
-
