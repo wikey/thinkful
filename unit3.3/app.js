48b6611b1d91590bb0a81178d0b7e7155c379825
Ian Sullivan <ian@churchkey.org>
2015-07-29 11:14:07 -0400

    broken build for debugging

--- unit3.3/app.js
+++ unit3.3/app.js
@@ -1,6 +1,34 @@
-var knownUniverse = [["1","H","hydrogen"],["2","He","helium"],["3","Li","Lithium"],["4","Be","Bererylium"],["5","B","Boron"],["6","C","Carbon"]]
+var knownUniverse = [["1","H","hydrogen"],["2","He","helium"],["3","Li","Lithium"],["4","Be","Bererylium"],["5","B","Boron"],["6","C","Carbon"]];
+var allE = [];
+var chosenE = undefined;
+
+function elementConst(num,abrev,name){
+    this.num = num;
+    this.abrev = abrev;
+    this.name = name;
+}
+
+function buildList(){
+    for (var i in knownUniverse){
+	/*	console.log(i);*/
+	var a = i[0];
+	var b = i[1];
+	var c = i[2];
+	console.log(a,b,c);
+	/*	console.log(b);*/
+	var newE = new elementConst(a,b,c);
+/*	console.log(newE);*/
+	allE.push(newE);
+    }
+}
+
+function pickE(){
+}
+
+function newGame(){
+}
 
 $(function(){
-    
+
     
 })
