634ce8720a868d153d34561194889da8b42b5eee
Ian Sullivan <ian@churchkey.org>
2015-07-31 07:56:02 -0400

    Where did I break the element abbrev list attaching to the different element divs?

--- unit3.3/app.js
+++ unit3.3/app.js
@@ -3,9 +3,10 @@
    ================== */
  
 /* ------------- Global variables */
-var knownUniverse = [["1","H","hydrogen"],["2","He","helium"],["3","Li","Lithium"],["4","Be","Bererylium"],["5","B","Boron"],["6","C","Carbon"]];
+var knownUniverse = [["1","H","hydrogen"],["2","He","helium"],["3","Li","Lithium"],["4","Be","Berylium"],["5","B","Boron"],["6","C","Carbon"]];
 var allE = [];
 var chosenE = undefined;
+var knownE = [];
 var eAbrevList = [];
 /* ------------- Convert our array of arrays into an array of objects */
 function element(anArray){ 
@@ -16,7 +17,12 @@ function element(anArray){
 
 /* ------------- Setup each item for quizzing */
 function pickE(){
-    chosenE = allE.splice(Math.floor(allE.length * Math.random()), 1)[0]; /*splice returns an array with the selected contents so we need the '[0]' at the end to set the variable to the actual object*/
+    if(allE.length < 0){
+	alert("You have mastered the elements! Press the 'New Game' button to play again.")
+    }
+    else{
+	chosenE = allE.splice(Math.floor(allE.length * Math.random()), 1)[0]; /*splice returns an array with the selected contents so we need the '[0]' at the end to set the variable to the actual object*/
+    }
 }
 
 function buildLists(){ 
@@ -27,7 +33,6 @@ function buildLists(){
     pickE();
     for(i in allE){
 	eAbrevList.push(allE[i].abrev);
-    }
 }
 
 
@@ -42,6 +47,7 @@ $(function(){
 /* ------------- Setup a new game */
     function newGame(){
       	allE = []; /* Clear values */
+	knownE = [];
 	eAbrevList = [];
 	$(".feedback p, .curE p, .elements p, .oldE p").empty(); /* should probably change the classes here to be on the <p> elements themselves and change where CSS positioning info */
       	buildLists(); /* Build new values */
@@ -49,10 +55,38 @@ $(function(){
 	$(".elements p").append(eAbrevList.join(", "));
     }
 
+/* ------------- Evaluate user guesses */
+
+    $("#guessForm").on("submit",function(e){
+	e.preventDefault();
+	var userGuess = $("#guessName").val();
+	console.log("userGuess is " + userGuess)
+	if(userGuess.toLowerCase() === chosenE.name.toLowerCase()){
+	    $(".feedback p").empty().append("That is right, ",chosenE.name," is the full name for '",chosenE.abrev,"'");  /*report win*/
+	    knownE.push(chosenE.abrev + " - " + chosenE.name + ", "); /*add chosenE to list of known elements*/
+	    $(".oldE p").append(knownE);
+	    $("#guessName").val('').empty();
+	    console.log("running pickE again"+chosenE.abrev);
+	    pickE(); /*run pickE again*/
+	    function listReset(){
+		eAbreveList = [];
+		for(i in allE){
+		    eAbrevList.push(allE[i].abrev);
+		}
+	    }
+/*	    $(".curE p").empty().append(chosenE.abrev);*/
+/*	    $(".elements p").empty().append(allE);	    */
+	    console.log("chosenE is now "+chosenE.abrev);
+	}
+	else{
+	    $(".feedback p").empty().append("Sorry ",userGuess," is not the full name for ",chosenE.abrev); /*update feedback*/
+	}
+    });
     
 /* ------------- Trigger new game on button press or page load*/
     $(".newgame").click(function(){
 	console.log("New game triggered by button");
 	newGame()});
     newGame();
+    console.log("page reloaded");
 })
