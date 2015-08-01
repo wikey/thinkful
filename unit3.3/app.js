ee58176af23aa0dc7d2c3189f949961b7ceca81b
Ian Sullivan <ian@churchkey.org>
2015-08-01 18:27:58 -0400

    resolved todo with trailing comma in known element list by making the items in knownE use an actual property of the objects and then using knownE.join to display them rather than including a custom string that included a final comma and including knownE raw.

--- unit3.3/app.js
+++ unit3.3/app.js
@@ -3,8 +3,6 @@
   - better colors
   - implement other training modes (start with full name or number and guess something else)
   - leave off comma on final element added do the mastered elements list
-  - make mastered elements a list of the actual elements instead of just the abbreviation string
-      - Does this mean I need a displayname method of the element object to present the "H - Hydrogen" version
   - Cleanup and combine all the functions
   - Add info about playing the game
   - Auto-focus on the form field on page load
@@ -17,8 +15,8 @@
    ================== */
  
 /* ------------- Global variables */
-var knownUniverse = [["1","H","hydrogen"],["2","He","helium"],["3","Li","Lithium"],["4","Be","Beryllium"],["5","B","Boron"],["6","C","Carbon"]];
-/*knownUniverse = [["1","H","hydrogen"],["2","He","helium"]];*/
+var knownUniverse = [["1","H","Hydrogen"],["2","He","Helium"],["3","Li","Lithium"],["4","Be","Beryllium"],["5","B","Boron"],["6","C","Carbon"]];
+/*knownUniverse = [["1","H","Hydrogen"],["2","He","Helium"]];*/
 var allE = [];
 var chosenE = undefined;
 var knownE = [];
@@ -28,6 +26,7 @@ function element(anArray){
     this.num = anArray[0];
     this.abrev = anArray[1];
     this.name = anArray[2];
+    this.displayInfo = this.abrev + " - " + this.name
 }
 
 /* ------------- Setup each item for quizzing */
@@ -72,8 +71,6 @@ $(function(){
 	$(".feedback p, .curE p, .elements p, .oldE p").empty(); /* should probably change the classes here to be on the <p> elements themselves and change where CSS positioning info */
       	buildLists(); /* Build new values */
 	listReset();
-/*      	$(".curE p").append(chosenE.abrev); /* Attach new values to html elements */
-/*	$(".elements p").append(eAbrevList.join(", "));*/
     }
 
 /* ------------- Evaluate user guesses */
@@ -84,8 +81,8 @@ $(function(){
 	console.log("userGuess is " + userGuess)
 	if(userGuess.toLowerCase() === chosenE.name.toLowerCase()){
 	    $(".feedback p").empty().append("That is right, ",chosenE.name," is the full name for '",chosenE.abrev,"'");  /*report win*/
-	    knownE.push(chosenE.abrev + " - " + chosenE.name + ", "); /*add chosenE to list of known elements*/
-	    $(".oldE p").empty().append(knownE);
+	    knownE.push(chosenE.displayInfo); /*add chosenE to list of known elements*/
+	    $(".oldE p").empty().append(knownE.join(", "));
 	    $("#guessName").val('').empty();
 	    console.log("running pickE again"+chosenE.abrev);
 	    if(allE.length < 1){
