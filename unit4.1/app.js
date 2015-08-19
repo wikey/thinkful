a46fe86adb74c8d5e4b9f864f030efe19a9bbe12
Ian Sullivan <ian@churchkey.org>
2015-08-18 22:49:10 -0400

    added optional position field to card sorting objectIfy function; added url for audio elements

--- unit4.1/app.js
+++ unit4.1/app.js
@@ -1,6 +1,6 @@
 function wikiParse(lesson){
     lessonId(lesson); //Lesson identifing
-    objectIfy(lesson.cards, 3); //Card processing
+    objectIfy(lesson.cards); //Card processing
 }
 
 function lessonId(lesson){
@@ -10,29 +10,31 @@ function lessonId(lesson){
 }
 
 function objectIfy(cardArray, cardNum){
-	// This level is the array of cards/arrays belonging to a page
-	var card_counter = 0;
-	for (var card in Object.keys(cardArray)){
-	    // This level is the array of sides/objects belonging to a single card
-	    card_counter += 1;
-	    var card_info = "<p>" + "Card " + card_counter + ":</br>" + "</p>";
-	    var side_counter = 0;
-	    var side_info = [];
-	    for (var side in Object.keys(cardArray[card])){
-		//This level is the collection of properties belonging to a single side/object of a single card
-		side_counter += 1;
-		console.log("side: " + side_counter);
-		side_info.push("side" + side_counter);
-		for (var property in cardArray[card][side]){
-		    console.log("property: " + property);
-		    side_info.push(property + ": " + cardArray[card][side][property]);
-		}//End Property
-	    }//End side
-	    $(".card").append("<h2>" + card_info + "</h2>" + "<p>" + side_info.join("<br>") + "</p>");
-	} //End card level
-    }//End cardArray
+    cards_to_search = cardArray[cardNum] || cardArray
+    // This level is the array of cards/arrays belonging to a page
+    var card_counter = 0;
+    for (var card in Object.keys(cards_to_search)){
+	// This level is the array of sides/objects belonging to a single card
+	card_counter += 1;
+	var card_info = "<p>" + "Card " + card_counter + ":</br>" + "</p>";
+	var side_counter = 0;
+	var side_info = [];
+	for (var side in Object.keys(cardArray[card])){
+	    //This level is the collection of properties belonging to a single side/object of a single card
+	    side_counter += 1;
+	    console.log("side: " + side_counter);
+	    side_info.push("side" + side_counter);
+	    for (var property in cardArray[card][side]){
+		console.log("property: " + property);
+		side_info.push(property + ": " + cardArray[card][side][property]);
+	    }//End Property
+	}//End side
+	$(".card").append("<h2>" + card_info + "</h2>" + "<p>" + side_info.join("<br>") + "</p>");
+    } //End card level
+}//End cardArray
+
 $(function(){
-        function fetch_and_clean_wiki(){
+    function fetch_and_clean_wiki(){
 	lesson_url = prompt("Give me your wikiotics lesson please.")
 	$.ajax({
 	    url: lesson_url + "?view=resource_jsonp",
