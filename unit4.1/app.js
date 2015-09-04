8c158e303233b5daf6dfde2ddad3b9eeee351a32
Ian Sullivan <ian@churchkey.org>
2015-09-03 21:17:50 -0400

    wapped individual cards in divs with a shared 'card' class and individually numbered IDs; split out sides of a card into their own divs with appropriate IDs and a shared 'side' class

--- unit4.1/app.js
+++ unit4.1/app.js
@@ -5,7 +5,7 @@ function wikiParse(lesson){
 
 function lessonId(lesson){
     var sideNumber = lesson.answer_columns.length + lesson.prompt_columns.length
-    $(".lessonInfo").append("<p><strong>Lesson Info</strong><br>" + "Groups: " + lesson.dividers.length + "<br>" + "Cards:" + lesson.cards.length + "<br>" + "Sides per card: " + sideNumber + "</p>");
+    $(".lessonInfo").append("<p><strong>Lesson Info</strong><br>" + "Groups: " + lesson.dividers.length + "<br>" + "Cards: " + lesson.cards.length + "<br>" + "Sides per card: " + sideNumber + "</p>");
 }
 
 function objectIfy(cardArray, cardNum){
@@ -15,20 +15,23 @@ function objectIfy(cardArray, cardNum){
     for (var card in Object.keys(cards_to_search)){
 	// This level is the array of sides/objects belonging to a single card
 	card_counter += 1;
-	var card_info = "<p>" + "Card " + card_counter + ":</br>" + "</p>";
+	var card_info = "Card " + card_counter + ":";
 	var side_counter = 0;
 	var side_info = [];
 	for (var side in Object.keys(cardArray[card])){
 	    //This level is the collection of properties belonging to a single side/object of a single card
 	    side_counter += 1;
 	    console.log("side: " + side_counter);
-	    side_info.push("side" + side_counter);
+//	    side_info = "<div id='" + "side" + side_counter + "'" + " class='side'" + ">" + "</div>";
+	    side_info.push("<div class='side'>" + "<h4>" + "side" + side_counter + "</h4>");
 	    for (var property in cardArray[card][side]){
 		console.log("property: " + property);
-		side_info.push(property + ": " + cardArray[card][side][property]);
+//Works but doesn't nest the divs inside the side div//		side_info.push("<div class='property'>" + property + ": " + cardArray[card][side][property] + "</div>");
+		side_info.push(property + ": " + cardArray[card][side][property] + "<br>");
 	    }//End Property
+	    side_info.push("</div>")
 	}//End side
-	$(".card").append("<h2>" + card_info + "</h2>" + "<p>" + side_info.join("<br>") + "</p>");
+	$(".cardGroup").append("<div class='card' id='c" + card_counter + "'>" + "<h2>" + card_info + "</h2>" + "<p>" + side_info.join("") + "</p>" + "</div>");
     } //End card level
 }//End cardArray
 
